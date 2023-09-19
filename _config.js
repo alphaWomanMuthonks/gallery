e00bce70badc66299b278fb208e00c6643f73485
Joy Malanga <81350129+alphaWomanMuthonks@users.noreply.github.com>
2023-09-19 15:02:44 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -1,10 +1,17 @@
 var config = {}
 
 // Update to have your correct username and password
+// config.mongoURI = {
+//     production: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/darkroom?retryWrites=true&w=majority',
+//     development: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+//     test: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+// }
+// module.exports = config;
+
 config.mongoURI = {
-    production: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://joymalanga:d2b3um5j8qvqeA7D@gallery.2khuvur.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
 
